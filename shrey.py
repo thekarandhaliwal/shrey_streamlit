92835f5a1f52fb8479138a94fb75a9453df364a9
thekarandhaliwal <141615125+thekarandhaliwal@users.noreply.github.com>
2024-06-21 14:17:46 +0530

    Update shrey.py

--- shrey.py
+++ shrey.py
@@ -8,11 +8,11 @@ from truedata.history import TD_hist
 import time
 
 def main():
-    st.title("DataFrame Display with Streamlit")
+    st.title("Values")
     
     # User input for x and y values
-    x = st.number_input('premium:', value=6000.0, format="%.2f")
-    y = st.number_input('Enter the value for y:', value=10.0, format="%.2f")
+    x = st.number_input('Excpected Premium:', value=6000.0, format="%.2f")
+    y = st.number_input('Percentage:', value=10.0, format="%.2f")
 
     if 'run_analysis' not in st.session_state:
         st.session_state.run_analysis = False
@@ -88,75 +88,64 @@ def main():
 
             a = [datetime.strptime(i[7], '%Y-%m-%dT%H:%M:%S') for i in response.json()['Records'] if i[11] == df.symbol.iloc[0]] if response.status_code == 200 else []
             b = sorted(set(a))
-            callp, putp, callstrike, putstrike, excpected, new = [], [], [], [], [], []
+            df['expected premium']=x/df.LOT
+            # callp, putp, callstrike, putstrike, excpected, new = [], [], [], [], [], []
 
-            for j, k, l, m, n in zip(df.symbol, df.call_price, df.put_price, df.LTP, df.LOT):
-                try:
-                    chain = my_analytics.get_option_chain(symbol=j, expiry=b[0], greeks=True)
-                    chain['strike'] = chain['strike'].astype(float)
-                    chain['civ'] = chain['civ'].astype(float)
-                    chain['piv'] = chain['piv'].astype(float)
-                    chain['putLTP'] = chain['putLTP'].astype(float)
-                    chain['callltp'] = chain['callltp'].astype(float)
-                    nearest_call = find_nearest(chain.strike.tolist(), k)
-                    callstrike.append(nearest_call)
-                    chain1 = chain[chain.strike == nearest_call]
-                    call_premium = chain1.callltp.iloc[0]
-                    callp.append(call_premium)
-                    nearest_put = find_nearest(chain.strike.tolist(), l)
-                    putstrike.append(nearest_put)
-                    chain2 = chain[chain.strike == nearest_put]
-                    put_premium = chain2.putLTP.iloc[0]
-                    putp.append(put_premium)
-                    nearest = find_nearest(chain.strike.tolist(), m)
-                    chain3 = chain[chain.strike == nearest]
-                    new.append(round(((chain3.civ.tolist()[0] + chain3.piv.tolist()[0]) / 2) * 100, 2))
-                    excpected_premium = round(float(exp) / float(n), 2)
-                    excpected.append(excpected_premium)
-                except:
-                    callp.append(0)
-                    putp.append(0)
-                    putstrike.append(0)
-                    new.append(0)
-                    excpected_premium = round(float(exp) / float(n), 2)
-                    excpected.append(excpected_premium)
-                    callstrike.append(0)
-                counter += 1
-                progress_bar.progress(min(counter / total_steps, 1.0))
+            # for j, k, l, m, n in zip(df.symbol, df.call_price, df.put_price, df.LTP, df.LOT):
+            #     try:
+            #         chain = my_analytics.get_option_chain(symbol=j, expiry=b[0], greeks=True)
+            #         chain['strike'] = chain['strike'].astype(float)
+            #         chain['civ'] = chain['civ'].astype(float)
+            #         chain['piv'] = chain['piv'].astype(float)
+            #         chain['putLTP'] = chain['putLTP'].astype(float)
+            #         chain['callltp'] = chain['callltp'].astype(float)
+            #         nearest_call = find_nearest(chain.strike.tolist(), k)
+            #         callstrike.append(nearest_call)
+            #         chain1 = chain[chain.strike == nearest_call]
+            #         call_premium = chain1.callltp.iloc[0]
+            #         callp.append(call_premium)
+            #         nearest_put = find_nearest(chain.strike.tolist(), l)
+            #         putstrike.append(nearest_put)
+            #         chain2 = chain[chain.strike == nearest_put]
+            #         put_premium = chain2.putLTP.iloc[0]
+            #         putp.append(put_premium)
+            #         nearest = find_nearest(chain.strike.tolist(), m)
+            #         chain3 = chain[chain.strike == nearest]
+            #         new.append(round(((chain3.civ.tolist()[0] + chain3.piv.tolist()[0]) / 2) * 100, 2))
+            #         excpected_premium = round(float(exp) / float(n), 2)
+            #         excpected.append(excpected_premium)
+            #     except:
+            #         callp.append(0)
+            #         putp.append(0)
+            #         putstrike.append(0)
+            #         new.append(0)
+            #         excpected_premium = round(float(exp) / float(n), 2)
+            #         excpected.append(excpected_premium)
+            #         callstrike.append(0)
+            #     counter += 1
+            #     progress_bar.progress(min(counter / total_steps, 1.0))
 
-            df['call premium'] = callp
-            df['put premium'] = putp
-            df['call strike'] = callstrike
-            df['put strike'] = putstrike
-            df['excpected premium'] = excpected
-            df['IV'] = new
-            df = df.drop(columns=['call_price', "put_price"])
-            df = df[['symbol', 'LOT', 'Previous', 'LTP', '%', 'change', 'call strike', 'put strike', 'excpected premium', 'call premium', 'put premium', 'IV']]
-            df.replace([pd.NA, float('inf'), float('-inf')], 0, inplace=True)
-            def custom_round(x):
-                if isinstance(x, float):
-                    return round(x, 2)
-                elif isinstance(x, int):
-                    return x
-                else:
-                    return x
-
-            # Apply this function to the DataFrame
-            df = df.applymap(custom_round)
-
-            # Display the rounded DataFrame
-            print(df)
-
+            # df['call premium'] = callp
+            # df['put premium'] = putp
+            # df['call strike'] = callstrike
+            # df['put strike'] = putstrike
+            # df['excpected premium'] = excpected
+            # df['IV'] = new
+            # df = df.drop(columns=['call_price', "put_price"])
+            # df = df[['symbol', 'LOT', 'Previous', 'LTP', '%', 'change', 'call strike', 'put strike', 'excpected premium', 'call premium', 'put premium', 'IV']]
+            # df.replace([pd.NA, float('inf'), float('-inf')], 0, inplace=True)
             counter += 1
             progress_bar.progress(min(counter / total_steps, 1.0))
 
             st.dataframe(df)
             counter += 1
             progress_bar.progress(min(counter / total_steps, 1.0))
-            current_time = datetime.now()
+            current_time = datetime.now().strftime('%H:%M:%S')
+
+# Print the current time as 11:00:00
 
             # Display the current time on Streamlit
-            st.write("Current Time:", current_time)
+            st.write("Last Updated:", current_time)
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
@@ -164,5 +153,5 @@ def main():
         elapsed_time = time.time() - start_time
         st.text(f"Total time taken: {elapsed_time:.2f} seconds")  # Display total time taken
     
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
