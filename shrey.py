f882aedc30a04e4cc23539d6c24d2ff4d0db1a07
thekarandhaliwal <141615125+thekarandhaliwal@users.noreply.github.com>
2024-06-21 16:49:09 +0530

    Update shrey.py

--- shrey.py
+++ shrey.py
@@ -1,3 +1,4 @@
+
 import logging
 from truedata.analytics import TD_analytics
 from datetime import datetime
@@ -88,71 +89,22 @@ def main():
 
             a = [datetime.strptime(i[7], '%Y-%m-%dT%H:%M:%S') for i in response.json()['Records'] if i[11] == df.symbol.iloc[0]] if response.status_code == 200 else []
             b = sorted(set(a))
-            df['expected premium']=x/df.LOT
-            # callp, putp, callstrike, putstrike, excpected, new = [], [], [], [], [], []
-
-            # for j, k, l, m, n in zip(df.symbol, df.call_price, df.put_price, df.LTP, df.LOT):
-            #     try:
-            #         chain = my_analytics.get_option_chain(symbol=j, expiry=b[0], greeks=True)
-            #         chain['strike'] = chain['strike'].astype(float)
-            #         chain['civ'] = chain['civ'].astype(float)
-            #         chain['piv'] = chain['piv'].astype(float)
-            #         chain['putLTP'] = chain['putLTP'].astype(float)
-            #         chain['callltp'] = chain['callltp'].astype(float)
-            #         nearest_call = find_nearest(chain.strike.tolist(), k)
-            #         callstrike.append(nearest_call)
-            #         chain1 = chain[chain.strike == nearest_call]
-            #         call_premium = chain1.callltp.iloc[0]
-            #         callp.append(call_premium)
-            #         nearest_put = find_nearest(chain.strike.tolist(), l)
-            #         putstrike.append(nearest_put)
-            #         chain2 = chain[chain.strike == nearest_put]
-            #         put_premium = chain2.putLTP.iloc[0]
-            #         putp.append(put_premium)
-            #         nearest = find_nearest(chain.strike.tolist(), m)
-            #         chain3 = chain[chain.strike == nearest]
-            #         new.append(round(((chain3.civ.tolist()[0] + chain3.piv.tolist()[0]) / 2) * 100, 2))
-            #         excpected_premium = round(float(exp) / float(n), 2)
-            #         excpected.append(excpected_premium)
-            #     except:
-            #         callp.append(0)
-            #         putp.append(0)
-            #         putstrike.append(0)
-            #         new.append(0)
-            #         excpected_premium = round(float(exp) / float(n), 2)
-            #         excpected.append(excpected_premium)
-            #         callstrike.append(0)
-            #     counter += 1
-            #     progress_bar.progress(min(counter / total_steps, 1.0))
-
-            # df['call premium'] = callp
-            # df['put premium'] = putp
-            # df['call strike'] = callstrike
-            # df['put strike'] = putstrike
-            # df['excpected premium'] = excpected
-            # df['IV'] = new
-            # df = df.drop(columns=['call_price', "put_price"])
-            # df = df[['symbol', 'LOT', 'Previous', 'LTP', '%', 'change', 'call strike', 'put strike', 'excpected premium', 'call premium', 'put premium', 'IV']]
-            # df.replace([pd.NA, float('inf'), float('-inf')], 0, inplace=True)
+            df['expected premium'] = x / df.LOT
             counter += 1
             progress_bar.progress(min(counter / total_steps, 1.0))
 
             df_numeric = df.drop('symbol', axis=1).applymap(lambda x: f"{x:.4f}")
             df_as_strings = df_numeric.applymap(lambda x: str(x))
             df_numeric = df_as_strings.applymap(lambda x: x[:-2])
-            df_numeric
+
             # Combine the numeric values DataFrame with the Symbols column
             df_combined = pd.concat([df['symbol'], df_numeric], axis=1)
-            df=df_combined
+            df = df_combined
             df = df.applymap(lambda x: x[:-3] if x[-2:] == '00' else x)
-            st.dataframe(df)
-            counter += 1
-            progress_bar.progress(min(counter / total_steps, 1.0))
+
+            st.dataframe(df)  # Display the dataframe only once
+
             current_time = datetime.now().strftime('%H:%M:%S')
-
-# Print the current time as 11:00:00
-
-            # Display the current time on Streamlit
             st.write("Last Updated:", current_time)
 
         except Exception as e:
@@ -163,3 +115,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
